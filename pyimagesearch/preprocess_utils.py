197372cadf2a2720b28fe7384f0c198bca75bd3f
Xuan Lin <willy418785@gmail.com>
2022-11-03 18:05:45 +0800

    :art: reformat code

--- pyimagesearch/preprocess_utils.py
+++ pyimagesearch/preprocess_utils.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
 
+
 class Config():
     filters = 32
     kernel_size = 3
+
 
 class SplitInputByDay(tf.keras.layers.Layer):
     def __init__(self, n_days, n_samples):
@@ -17,16 +19,18 @@ class SplitInputByDay(tf.keras.layers.Layer):
         # x = tf.ensure_shape(x, [None, self.n_days, self.n_samples, channels])
         return x
 
+
 class MultipleDaysConvEmbed(tf.keras.layers.Layer):
     def __init__(self, filters, filter_size, n_days, n_samples):
         super().__init__()
         self.filters = filters
         self.filter_size = filter_size
-        self.pad_size = int(filter_size/2)
+        self.pad_size = int(filter_size / 2)
         self.n_days = n_days
         self.n_samples = n_samples
         self.pad = tf.keras.layers.ZeroPadding2D(padding=(0, self.pad_size))
-        self.embed = tf.keras.layers.Conv2D(filters=filters, kernel_size=(n_days, filter_size), strides=1, padding='valid',activation='elu')
+        self.embed = tf.keras.layers.Conv2D(filters=filters, kernel_size=(n_days, filter_size), strides=1,
+                                            padding='valid', activation='elu')
         self.reshape = tf.keras.layers.Reshape((n_samples, -1))
 
     def call(self, inputs, training):
@@ -36,6 +40,7 @@ class MultipleDaysConvEmbed(tf.keras.layers.Layer):
         x = self.reshape(x, training=training)
         # x = tf.ensure_shape(x, [None, self.n_samples, channels])
         return x
+
 
 if __name__ == '__main__':
     model = tf.keras.Sequential([SplitInputByDay(2, 10), MultipleDaysConvEmbed(10, 5, 2, 10)])
