2cb7ae83aec9f69d477142ad2106a9e03d35e640
Xuan Lin <willy418785@gmail.com>
2022-12-19 18:04:39 +0800

    :necktie: change specification logic of sequence sampling stride

--- pyimagesearch/windowsGenerator.py
+++ pyimagesearch/windowsGenerator.py
@@ -453,7 +453,7 @@ class WindowGenerator():
     def trainData(self, sepMode: str = "all", addcloud=False):
         # return self.input_dataset_2(self.trainDataX, self.trainCloudX, self.trainY_nor, addcloud=addcloud,
         #                             sequence_stride=1, use_shuffle=parameter.is_using_shuffle)
-        if self.is_sampling_within_day:
+        if self.is_sampling_within_day or parameter.input_days is None:
             if addcloud:
                 return self.input_dataset(self.trainDataX, self.trainY_nor, cloudData=self.trainCloudX,
                                           sequence_stride=1, use_shuffle=parameter.is_using_shuffle)
@@ -470,7 +470,7 @@ class WindowGenerator():
 
     def valData(self, sepMode: str = "all", addcloud=False):
         # return self.input_dataset_2(self.valDataX, self.valCloudX, self.valY_nor, addcloud=addcloud, sequence_stride=1)
-        if self.is_sampling_within_day:
+        if self.is_sampling_within_day or parameter.input_days is None:
             if addcloud:
                 return self.input_dataset(self.valDataX, self.valY_nor, cloudData=self.valCloudX,
                                           sequence_stride=1)
