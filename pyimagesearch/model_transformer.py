b8ebba4999007697d5549532ea769ec0b4837a19
Xuan Lin <willy418785@gmail.com>
2022-12-28 17:38:56 +0800

    :sparkles: Transformer can now take timestamp embeddings

--- pyimagesearch/model_transformer.py
+++ pyimagesearch/model_transformer.py
@@ -9,7 +9,7 @@ from tensorflow.keras.layers import Conv1D, MaxPooling1D, GRU, LayerNormalizatio
     MultiHeadAttention
 from tensorflow.keras.models import Model
 
-from pyimagesearch import parameter
+from pyimagesearch import parameter, time_embedding
 from pyimagesearch.datautil import DataUtil
 from pyimagesearch.windowsGenerator import WindowGenerator
 
@@ -95,12 +95,13 @@ class Encoder(tf.keras.layers.Layer):
 
         self.dropout = tf.keras.layers.Dropout(rate)
 
-    def call(self, x, is_pooling, training, mask):
+    def call(self, x, is_pooling, training, mask, timestamp_embedding=None):
         # adding embedding and position encoding.
         x = self.embedding(x)  # (batch_size, input_seq_len, d_model)
         x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
         x += self.pos_encoding[:, :self.seq_len, :]
-
+        if timestamp_embedding is not None:
+            x += timestamp_embedding
         x = self.dropout(x, training=training)
 
         for i in range(self.num_layers):
@@ -153,12 +154,13 @@ class Decoder(tf.keras.layers.Layer):
             for _ in range(num_layers)]
         self.dropout = Dropout(rate)
 
-    def call(self, x, enc_output, is_pooling, training):
+    def call(self, x, enc_output, is_pooling, training, timestamp_embedding=None, iter_step=None):
         seq_len = tf.shape(x)[1]
         x = self.embedding(x)  # (batch_size, target_seq_len, d_model)
         x *= tf.math.sqrt(tf.cast(self.d_model, tf.float32))
         x += self.pos_encoding[:, :seq_len, :]
-
+        if timestamp_embedding is not None:
+            x += timestamp_embedding if iter_step is None else timestamp_embedding[:, :iter_step + 1, :]
         x = self.dropout(x, training=training)
 
         for i in range(self.num_layers):
@@ -170,7 +172,7 @@ class Decoder(tf.keras.layers.Layer):
 
 class Transformer(tf.keras.Model):
     def __init__(self, num_layers, d_model, num_heads, dff, src_seq_len, tar_seq_len, src_dim, tar_dim, rate=0.1,
-                 gen_mode="unistep", is_seq_continuous=False, is_pooling=False, token_len = None):
+                 gen_mode="unistep", is_seq_continuous=False, is_pooling=False, token_len=None):
         assert gen_mode in gen_modes
         super().__init__()
         self.d_model = d_model
@@ -185,12 +187,15 @@ class Transformer(tf.keras.Model):
                                num_heads=num_heads, dff=dff,
                                seq_len=src_seq_len, rate=rate)
         if self.gen_mode == 'unistep':
-            assert type(token_len) is int
-            assert src_seq_len >= token_len
-            self.token_len = token_len
+            if token_len is not None and is_seq_continuous:
+                assert type(token_len) is int
+                assert src_seq_len >= token_len
+                self.token_len = token_len
+            else:
+                self.token_len = 0
             self.decoder = Decoder(num_layers=num_layers, d_model=d_model,
                                    num_heads=num_heads, dff=dff,
-                                   seq_len=token_len+tar_seq_len, rate=rate)
+                                   seq_len=token_len + tar_seq_len, rate=rate)
         elif self.gen_mode == 'auto':
             self.decoder = Decoder(num_layers=num_layers, d_model=d_model,
                                    num_heads=num_heads, dff=dff,
@@ -200,14 +205,19 @@ class Transformer(tf.keras.Model):
         self.final_layer = Dense(tar_dim)
         # self.build(input_shape=(None, src_seq_len, src_dim))
 
-    def call(self, inputs, training):
-        enc_out = self.encoder(inputs, self.is_pooling, training, mask=None)
+    def call(self, inputs, training, time_embedding_tuple=None):
+        src_timestamps, shift_timestamps, tar_timestamps = time_embedding_tuple if time_embedding_tuple is not None else (
+            None, None, None)
+        enc_out = self.encoder(inputs, self.is_pooling, training, mask=None, timestamp_embedding=src_timestamps)
         if self.gen_mode == 'unistep':
             tar = tf.stack([tf.shape(inputs)[0], self.tar_seq_len, self.src_dim])
             tar = tf.fill(tar, 0.0)
-            if self.token_len > 0:
+            decoder_timestamps = tar_timestamps
+            if self.token_len > 0 and self.is_seq_continuous:
                 tar = tf.concat([inputs[:, -self.token_len:, :], tar], axis=1)
-            dec_out = self.decoder(tar, enc_out, False, training)
+                if time_embedding_tuple is not None:
+                    decoder_timestamps = tf.concat([src_timestamps[:, -self.token_len:, :], tar_timestamps], axis=1)
+            dec_out = self.decoder(tar, enc_out, False, training, timestamp_embedding=decoder_timestamps)
             out = self.final_layer(dec_out)
             out = out[:, -self.tar_seq_len:, :]
         elif self.gen_mode == 'auto':
@@ -219,7 +229,8 @@ class Transformer(tf.keras.Model):
                     tf.autograph.experimental.set_loop_options(
                         shape_invariants=[(tar, tf.TensorShape([None, None, self.d_model]))])
 
-                    dec_out = self.decoder(tar, enc_out, False, training)
+                    dec_out = self.decoder(tar, enc_out, is_pooling=False, training=training,
+                                           timestamp_embedding=tar_timestamps, iter_step=i)
 
                     tar = tf.concat([tar, dec_out[:, -1:, :]], axis=1)
                 out = self.final_layer(tar[:, 1:, :])
@@ -249,13 +260,14 @@ if __name__ == '__main__':
                                       feature_col=parameter.features,
                                       split_mode=parameter.split_mode,
                                       month_sep=parameter.test_month)
-    src_len = 540
-    tar_len = 540
     dataUtil = data_with_weather_info
+    src_len = 15
+    shift = 5
+    tar_len = 10
     w2 = WindowGenerator(input_width=src_len,
                          image_input_width=0,
                          label_width=tar_len,
-                         shift=parameter.after_minutes,
+                         shift=shift,
 
                          trainImages=dataUtil.trainImages,
                          trainData=dataUtil.train_df[dataUtil.feature_col],
@@ -275,22 +287,30 @@ if __name__ == '__main__':
                          testAverage=dataUtil.test_df_average,  ######
                          testY=dataUtil.test_df[dataUtil.label_col],
 
-                         batch_size=parameter.batchsize,
+                         batch_size=32,
                          label_columns="ShortWaveDown",
-                         samples_per_day=dataUtil.samples_per_day)
-    model = Transformer(num_layers=3, d_model=8, num_heads=4, dff=32, src_seq_len=src_len, tar_seq_len=tar_len,
-                        src_dim=len(parameter.features),
-                        tar_dim=len(parameter.target), rate=0.1, gen_mode="unistep", is_seq_continuous=True,
-                        is_pooling=True, token_len=0)
+                         samples_per_day=dataUtil.samples_per_day,
+                         using_timestamp_data=True)
+    input_scalar = Input(shape=(src_len, len(parameter.features)))
+    input_time = Input(shape=(src_len + shift + tar_len, len(time_embedding.vocab_size)))
+    embedding = time_embedding.TimeEmbedding(output_dims=Config.d_model, input_len=src_len, shift_len=shift,
+                                             label_len=tar_len)(input_time)
+    LR = Transformer(num_layers=Config.layers, d_model=Config.d_model, num_heads=Config.n_heads, dff=Config.dff,
+                     src_seq_len=src_len, tar_seq_len=tar_len,
+                     src_dim=len(parameter.features),
+                     tar_dim=len(parameter.target), rate=0.1, gen_mode="unistep", is_seq_continuous=True,
+                     is_pooling=True, token_len=5)(input_scalar, time_embedding_tuple=embedding)
+
+    model = Model(inputs=[input_scalar, input_time], outputs=LR)
     model.compile(loss=tf.losses.MeanSquaredError(), optimizer="Adam"
                   , metrics=[tf.metrics.MeanAbsoluteError()
             , tf.metrics.MeanAbsolutePercentageError()])
-
-    # model.summary()
-    # tf.keras.backend.clear_session()
-    # history = model.fit(w2.trainData(addcloud=parameter.addAverage),
-    #                     validation_data=w2.valData(addcloud=parameter.addAverage),
-    #                     epochs=100, batch_size=parameter.batchsize, callbacks=[parameter.earlystoper])
+    model.summary()
+    tf.keras.backend.clear_session()
+    history = model.fit(w2.trainData(addcloud=parameter.addAverage),
+                        validation_data=w2.valData(addcloud=parameter.addAverage),
+                        epochs=100, batch_size=5, callbacks=[parameter.earlystoper])
     for x, y in w2.trainData(addcloud=parameter.addAverage):
-        c = model(x[0:1, :, :])
-        pass
+        c = model(x)
+    model.summary()
+    pass
