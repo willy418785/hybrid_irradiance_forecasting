cb07703e421183e41c9f552cebe49345fee27320
Xuan Lin <willy418785@gmail.com>
2023-01-27 23:28:36 +0800

    config🔧: update parameter.py

--- pyimagesearch/parameter.py
+++ pyimagesearch/parameter.py
@@ -10,14 +10,14 @@ static_suffle = False
 dynamic_suffle = False
 timeseries = True
 datasetPath = "../skyImage"
-csv_name = 'ElectricityConsumption_2012-2014.csv'  # ["2020final.csv","2020new","2020shuffleDay", 'dataset_renheo_[2019].csv', 'ElectricityConsumption_2012-2014.csv']
+csv_name = 'original_EC.csv'  # ["2020final.csv","2020new","2020shuffleDay", 'dataset_renheo_[2019].csv', 'ElectricityConsumption_2012-2014.csv']
 # features = ['ShortWaveDown'] # target only
 # features = ['ShortWaveDown', 'CWB_Humidity', 'CWB_Temperature']  # david suggested
 # features = ["DC-1|Pdc", "DC-2|Pdc", "Temperature", "RH"] # david suggested
 # features = None
 # features = ["MT_001"]
-features = ["MT_{}".format(str(i).zfill(3)) for i in range(1, 371)]
-target = ["MT_{}".format(str(i).zfill(3)) for i in range(1, 371)]
+features = ["MT_{}".format(str(i).zfill(3)) for i in range(1, 322)]
+target = ["MT_{}".format(str(i).zfill(3)) for i in range(1, 322)]
 
 # target = ["MT_001"]
 # target = None   # "ShortWaveDown","difference5","difference10", ["DC-1|Pdc","DC-2|Pdc"]
