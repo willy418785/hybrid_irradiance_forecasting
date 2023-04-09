a4ebfe1dfa887638f8e0765b4704fe37c98d71ce
Xuan Lin <willy418785@gmail.com>
2023-04-09 18:14:23 +0800

    refactor♻️: integrate figure plotting code

--- test.py
+++ test.py
@@ -8,54 +8,51 @@ from pathlib import Path
 
 '''
 Plot performance bar chart of multiple experiments for visual comparison.
-Note that all tested models should be included after looking up every specified all_metrics.csv, otherwise, it would get assertion failure.
 -------------------------------------------------------------------------
 metric_names:    list<str>, contains names of metrics that is desired to be plotted
+models:          list<str>, list of all tested models, include all models been read when not specified(None)
 labels:          list<str>, contains dir names in ./plot that have all_metrics.csv located
-experiment_name: str, the name of dir in ./fig where all resulting figures belong
-rex:             str, regular expression for filtering wanted metrics
+alt_labels:      list<str>, alternative name for each element in labels, should have the same length as that of labels
+col_num_per_plot:      int, number of columns per figure(for cleaner figure)
+experiment_name:       str, the name of dir in ./fig where all resulting figures belong
+rex:                   str, regular expression for filtering wanted metrics
 '''
 
 if __name__ == '__main__':
-    metric_names = ["MSE", "RMSE", "RMSPE", "MAE", "MAPE", "WMAPE", "VWMAPE", "corr"]
-    # labels = ["8to15_hourly_day2day_david_suggested_weather_data"]
-    # labels = ["ma_smoothed_auto_vs_unistep_convGRU_vs_transformer_target_data_only_5_to_10"]
-    # labels = ["ma_smoothed_auto_vs_unistep_convGRU_vs_transformer_david_suggested_weather_data_only_5_to_10"]
-    # labels = ["ma_smoothed_auto_convGRU_vs_auto_8_4_3_transformer_numerical_weather_data_only_5_to_10", "ma_smoothed_convGRU_vs_8_4_3_transformer_numerical_weather_data_only_5_to_10"]
-    # labels = ['ma_smoothed_8_4_3_transformer_numerical_weather_data_only_540_to_540_train_on_all_months', 'ma_smoothed_convGRU_numerical_weather_data_only_540_to_540_train_on_all_months']
-    # labels = ['ma_smoothed_unistep_transformer_target_data_only_540_to_540',
-    #           'ma_smoothed_unistep_convGRU_target_data_only_540_to_540']
-    # labels = ['ma_smoothed_transformer_david_suggested_weather_data_only_540_to_540',
-    #           'ma_smoothed_convGRU_david_suggested_weather_data_only_540_to_540']
-    # labels = ["8to15_hourly_day2day_renheo[2019]_david_suggested_weather_data"]
-    labels = ["hourly_10day5day_AR_EC"]
-    # experiment_name = "david_suggested_weather_data_only_5_to_10"
-    # experiment_name = "ma_smoothed_weather_data_only_540_to_540"
-    # experiment_name = "ma_smoothed_david_suggested_weather_data_only_540_to_540"
-    # experiment_name = "ma_smoothed_target_data_only_540_to_540"
-    experiment_name = "hourly_10day5day_AR_EC_by_day"
-    # experiment_name = '8to15_hourly_day2day_renheo[2019]_david_suggested_weather_data_hours_separated'
-    # experiment_name = '8to15_hourly_day2day_david_suggested_weather_data_hours_separated'
-    # model_names = ["Persistence", "MA", "datamodel_CL", "autoregressive_convGRU", "simple_transformer", "autoregressive_transformer"]
-    # model_names = ["Persistence", "MA", "convGRU", 'convGRU_w_mlp_decoder', "autoregressive_convGRU", "transformer",
-    #                'transformer_w_mlp_decoder',"autoregressive_transformer"]
-    model_names = ["Persistence", "MA", "AR", 'channelwise_AR']
-    # model_names = ["Persistence", "MA", "datamodel_CL", "simple_transformer"]  # tested models
-    # rex = '$'
-    # rex = '{1}.+min$'
-    rex = '{1}.+th day$'
+    metric_names = ["MSE", "RMSE", "RMSPE", "MAE", "MAPE", "WMAPE", "RSE", "VWMAPE", "corr"]
+    # models = None
+    # models = ["Persistence", "MA"]  # baselines only
+    models = ["convGRU", 'stationary_convGRU', 'znorm_convGRU',
+              "transformer", "stationary_transformer", 'znorm_transformer']  # models only
+    # models = ["transformer", "stationary_transformer", 'znorm_transformer']    # transformer only
+    # models = ["convGRU", 'stationary_convGRU', 'znorm_convGRU'] # convGRU only
+    base = "layers[{}]_EC_i168s0o168_rate24trate24_norm[std]scale[None]_bypass[None]TE[None]split[False]"
+    labels = [base.format(i) for i in range(1, 9)]
+    alt_labels = ["Layers-{}".format(i) for i in range(1, 9)]
+    col_num_per_plot = 10
+    # experiment_name = "test"
+    experiment_name = "layers_ablation"
+    rex = '$'
+
     try:
-        os.mkdir(Path("fig"))
+        assert len(alt_labels) == len(labels)
+        alt_labels = dict(zip(labels, alt_labels))
+    except Exception as ex:
+        alt_labels = dict(zip(labels, labels))
+    max_index_len = max([len(alt_labels[k]) for k in alt_labels])
+    base_path = os.path.sep.join(["fig"]) if len(labels) == 1 else os.path.sep.join(["fig", "ablation"])
+    try:
+        os.mkdir(base_path)
     except:
         pass
     try:
-        os.mkdir(Path("fig/{}".format(experiment_name)))
+        os.mkdir(os.path.sep.join([base_path, experiment_name]))
     except:
         pass
     for name in metric_names:
         all_months = pd.DataFrame()
         try:
-            os.mkdir(Path("fig/{}/{}".format(experiment_name, name)))
+            os.mkdir(os.path.sep.join([base_path, experiment_name, name]))
         except:
             pass
         for month in range(1, 13):
@@ -63,8 +60,6 @@ if __name__ == '__main__':
             mydate = datetime.datetime.strptime(str(month), "%m")
             month_name = mydate.strftime("%B")
             month_abbr = mydate.strftime("%b")
-            title = "Test on {}".format(month_name)
-            fig_path = "fig/{}/{}/{}".format(experiment_name, name, title)
             for label in labels:
                 experiment_label = label + "_test_on_{}".format(month_abbr)
                 path = "plot/{}/{}".format(experiment_label, "all_metric.csv")
@@ -75,51 +70,62 @@ if __name__ == '__main__':
                 try:
                     metrics = pd.read_csv(path, index_col=0)
                     metric = metrics.filter(regex='^' + name + rex, axis='columns').round(5)
-                    models = list((set(model_names) & set(list(metric.index))) - set(all.index))
-                    models = [ele for ele in model_names if ele in models]
-                    metric = metric.loc[models]
-                    all = pd.concat([all, metric], axis=0)
-                    pass
+                    metric = pd.concat({alt_labels[label]: metric}, names=['exp_name', 'models'])
+                    metric.index = metric.index.swaplevel(0, 1)
+                    # metric = metric.transpose()
+                    # metric = metric.rename({col: "{}\n{}".format(label, col) for col in metric.columns if col not in exclusive_models}, axis=1)
+                    # metric = metric.transpose()
+                    all = all.append(metric)
                 except Exception as ex:
                     print(str(ex))
                     break
-            if len(all) == len(model_names):
-                all = all.reindex(model_names)
+            if len(all) > 0:
                 if is_monthly_result_existing:
-                    ax = plt.subplot(111)
-                    pos = ax.get_position()
-                    ax.set_position([pos.x0, pos.y0 + 0.2 * (pos.y1 - pos.y0), pos.x1 - pos.x0, 0.8 * (pos.y1 - pos.y0)])
-                    all.plot.bar(title=title, ax=ax, legend=True, table=True, figsize=(10, 6), layout="constrained")
-                    x_axis = ax.axes.get_xaxis()
-                    x_axis.set_visible(False)
-                    ax.legend(loc='lower right', fontsize=5)
-                    plt.savefig(fig_path)
-                    plt.clf()
+                    unstacked = all.unstack().droplevel(0, axis=1)[alt_labels.values()]
+                    unstacked = unstacked.reindex(models) if models is not None else unstacked
+                    for i, v in enumerate(range(0, len(unstacked), col_num_per_plot)):
+                        tmp = unstacked[v:v + col_num_per_plot]
+                        title = "{} Test on {}".format(name, month_name)
+                        fig_path = os.path.sep.join([base_path, experiment_name, name, "{}_{}".format(title, i)])
+                        fig, axs = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]},
+                                                figsize=(2 * tmp.shape[0], 9 + .1 * tmp.shape[1]))
+                        axs[1].axis('off')
+                        axs[1].axis('tight')
+                        ax = axs[0]
+                        tmp.plot.bar(title=title, ax=ax, legend=True)
+                        ax.axes.get_xaxis().set_visible(False)
+                        t = tmp.transpose()
+                        pd.plotting.table(ax, t, loc='bottom', rowLoc='right', colLoc='center')
+                        ax.legend(loc='lower right', fontsize=5)
+                        # plt.show()
+                        plt.savefig(fig_path)
+                        plt.clf()
                 all.columns = pd.MultiIndex.from_product([all.columns, [month_name]])
                 all_months = pd.concat([all_months, all], axis=1)
                 if not is_monthly_result_existing:
                     break
-                pass
         assert len(all_months.columns) > 0
-        mean = all_months.groupby(axis=1, level=0).mean().round(3)
-        # mean.columns = pd.MultiIndex.from_product([['mean'], mean.columns])
-        std_err = all_months.groupby(axis=1, level=0).sem().round(3)
-        # std_err.columns = pd.MultiIndex.from_product([['SE'], std_err.columns])
-        # mean = pd.DataFrame(all_months.mean(axis=1).round(5), columns=[name])
-        # std_err = pd.DataFrame(all_months.sem(axis=1).round(5), columns=["SE"])
-        title = 'Mean with SE'
-        fig_path = "fig/{}/{}/{}".format(experiment_name, name, title)
-        ax = plt.subplot(111)
-        pos = ax.get_position()
-        ax.set_position([pos.x0, pos.y0+0.2*(pos.y1-pos.y0), pos.x1-pos.x0, 0.8*(pos.y1-pos.y0)])
-        mean.plot.bar(yerr=std_err, capsize=3, title=title, ax=ax, figsize=(10, 6), legend=True, layout="constrained")
-        x_axis = ax.axes.get_xaxis()
-        x_axis.set_visible(False)
-        ax.legend(loc='lower right', fontsize=5)
-        # ax = plt.subplot(212)
-        # ax.set_axis_off ()
-        t = (mean.astype(str) + '±' + std_err.astype(str)).transpose()
-        pd.plotting.table(ax, t, loc='bottom', rowLoc='right')
-        # plt.show()
-        plt.savefig(fig_path)
-        plt.clf()
+        mean = all_months.groupby(axis=1, level=0).mean().round(3).unstack().droplevel(0, axis=1)[alt_labels.values()]
+        mean = mean.reindex(models) if models is not None else mean
+        std_err = all_months.groupby(axis=1, level=0).sem().round(3).unstack().droplevel(0, axis=1)[alt_labels.values()]
+        std_err = std_err.reindex(models) if models is not None else std_err
+        for i, v in enumerate(range(0, len(mean), col_num_per_plot)):
+            mean_tmp = mean[v:v + col_num_per_plot]
+            std_tmp = std_err[v:v + col_num_per_plot]
+            title = ('Mean {}'.format(name) + "" if std_tmp.isna().any().any() else " with SE")
+            fig_path = os.path.sep.join([base_path, experiment_name, name, "{}_{}".format(title, i)])
+            fig, axs = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]},
+                                    figsize=(2 * mean_tmp.shape[0], 9 + .1 * mean_tmp.shape[1]))
+            axs[1].axis('off')
+            axs[1].axis('tight')
+            ax = axs[0]
+            mean_tmp.plot.bar(yerr=std_tmp, capsize=3, title=title, ax=ax,
+                              legend=True, layout="constrained")
+            ax.axes.get_xaxis().set_visible(False)
+            ax.legend(loc='lower right', fontsize=5)
+            t = (mean_tmp.astype(str) + ("" if std_tmp.isna().any().any() else'±' + std_err.astype(str))).transpose()
+            pd.plotting.table(ax, t, loc='bottom', rowLoc='right', colLoc='center')
+            # plt.show()
+            plt.savefig(fig_path)
+            plt.clf()
+pass
